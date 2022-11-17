3af7096d43ba7c563fbdf72b7597c13137ccba6a
yichi170 <ilee900620@gmail.com>
2022-11-17 19:27:39 +0800

    [fix] can only choose one likert scale & try react-hook-form

--- frontend/src/components/likert.js
+++ frontend/src/components/likert.js
@@ -4,11 +4,11 @@ function Likert(props) {
             <h3 className="text-lg">{props.question}</h3>
             <ul className="likert flex">
                 <li> Strongly Disagree </li>
-                <li><input className="flex-auto" type="radio" name="agree" value="-2" /></li>
-                <li><input className="flex-auto" type="radio" name="agree" value="-1" /></li>
-                <li><input className="flex-auto" type="radio" name="agree" value="0" /></li>
-                <li><input className="flex-auto" type="radio" name="agree" value="1" /></li>
-                <li><input className="flex-auto" type="radio" name="agree" value="2" /></li>
+                <li><input className="flex-auto" type="radio" name={props.question} value="-2"/></li>
+                <li><input className="flex-auto" type="radio" name={props.question} value="-1"/></li>
+                <li><input className="flex-auto" type="radio" name={props.question} value="0"/></li>
+                <li><input className="flex-auto" type="radio" name={props.question} value="1"/></li>
+                <li><input className="flex-auto" type="radio" name={props.question} value="2"/></li>
                 <li> Strongly Agree </li>
             </ul>
         </div>
--- frontend/src/components/questionnaire.js
+++ frontend/src/components/questionnaire.js
@@ -1,11 +1,17 @@
+import { useForm } from "react-hook-form";
 import Likert from "./likert";
 
 function Quest() {
+    const { register, handleSubmit } = useForm();
+    const onSubmit = (data) => console.log(data);
+    
     return (
-        <div>
-            <Likert question="Q1"/>
-            <Likert question="Q2"/>
-        </div>
+        <form onSubmit={handleSubmit(onSubmit)}> 
+            <Likert question="Q1" {...register("q1")}/>
+            <Likert question="Q2" {...register("q2")}/>
+
+            <input type="submit" className="bg-blue-500 active:bg-blue-600"/>
+        </form>
     );
 }
 
