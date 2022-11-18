856073f40be4a4215470df7211711ca742709bd9
james5418 <james900425@gmail.com>
2022-11-18 19:51:12 +0800

    [feat] add CORS in backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
 from routers.summary import summary_router
@@ -7,6 +8,14 @@ from routers.login import login_router
 app = FastAPI()
 app.include_router(summary_router, prefix="/summary")
 app.include_router(login_router, prefix="/login")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 if __name__ == '__main__':
     uvicorn.run("main:app", host="0.0.0.0", port=5000, reload=True)
