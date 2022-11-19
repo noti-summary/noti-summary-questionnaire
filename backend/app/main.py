5a4e13df78333f362c3fbf56c0ba88dd053f8608
yichi170 <ilee900620@gmail.com>
2022-11-20 00:11:46 +0800

    [refactor] change from 5000 to 5001, since the goddamn Mac hijacked my 5000 port

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,4 +9,4 @@ app.include_router(summary_router, prefix="/summary")
 app.include_router(login_router, prefix="/login")
 
 if __name__ == '__main__':
-    uvicorn.run("main:app", host="0.0.0.0", port=5000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=5001, reload=True)
