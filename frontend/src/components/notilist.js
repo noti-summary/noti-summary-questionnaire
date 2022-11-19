a2bb50cbf464cd748c6733632c111dcfff0c43a0
yichi170 <ilee900620@gmail.com>
2022-11-19 09:56:57 +0800

    [feat] pass noti from dynamic route

--- frontend/src/components/notilist.js
+++ frontend/src/components/notilist.js
@@ -1,43 +1,10 @@
 import axios from 'axios';
 import NotiCard from './notiCard';
-import React, { useState, useEffect } from 'react';
 
-const dataURL = "http://0.0.0.0:8000/summary/000/123";
-
-const testData = [
-    {
-        "appName": "Messenger",
-        "category": "msg",
-        "content": "👍",
-        "notificationId": "000_1667459329893",
-        "time": "15:08",
-        "title": "Message",
-        "userId": "000"
-    },
-    {
-        "appName": "Messenger",
-        "category": "msg",
-        "content": "👍",
-        "notificationId": "000_1667459329893",
-        "time": "15:08",
-        "title": "Message",
-        "userId": "000"
-    }
-];
-
-function NotiList() {
-    // const [notiData, setNoti] = useState(null);
-    // useEffect(() => {
-    //     axios.get(dataURL).then((res) => {
-    //         setNoti(res['notification']);
-    //     })
-    // }, []);
-
-    // if (!notiData)  return null;
-    
+function NotiList(props) {
     return (
         <div>
-            {testData.map(noti => <NotiCard {...noti} key={noti.notificationId}/>)}
+            {props.notis.map(noti => <NotiCard {...noti} key={noti.notificationId}/>)}
         </div>
     );
 }
--- frontend/src/pages/[userId]/[summaryId]/index.js
+++ frontend/src/pages/[userId]/[summaryId]/index.js
@@ -1,4 +1,6 @@
-import { useRouter } from 'next/router'
+import { useRouter } from 'next/router';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 import NotiList from '../../../components/notiList';
 import Quest from '../../../components/questionnaire';
 
@@ -7,11 +9,27 @@ export default function Questionnaire() {
     const router = useRouter();
     const {userId, summaryId} = router.query
 
+    
+
+    const [notiData, setNoti] = useState(null);
+    const [summary, setSum] = useState(null);
+    
+    useEffect(() => {
+        const dataURL = `http://0.0.0.0:8000/summary/001/123`; // unable to use userId & summaryId directly
+        console.log(dataURL);
+        axios.get(dataURL).then((res) => {
+            setNoti(res['data']['notification']);
+            setSum(res['data']);
+        })
+    }, [userId, summaryId]);
+
+    if (!notiData)  return null;
+
     return(
         <div>
             <h1>This is /{userId}/{summaryId}</h1>
             <div className="grid gap-x-96 grid-cols-2">
-                <NotiList/>
+                <NotiList notis={notiData}/>
                 <Quest/>
             </div>
         </div>
