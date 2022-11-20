c6a22596295e704844f06ea91c23eff5a853a5ca
yichi170 <ilee900620@gmail.com>
2022-11-20 20:40:08 +0800

    [fix] react-hook-form is now able to get the result of form

--- frontend/src/components/questionnaire.js
+++ frontend/src/components/questionnaire.js
@@ -3,12 +3,14 @@ import Likert from "./likert";
 
 function Quest() {
     const { register, handleSubmit } = useForm();
-    const onSubmit = (data) => console.log(data);
+    const onSubmit = (data) => {
+        console.log(data)
+    };
     
     return (
         <form onSubmit={handleSubmit(onSubmit)}> 
-            <Likert question="Q1" {...register("q1")}/>
-            <Likert question="Q2" {...register("q2")}/>
+            <Likert question="Q1" reg={register("q1")}/>
+            <Likert question="Q2" reg={register("q2")}/>
 
             <input type="submit" className="bg-blue-500 active:bg-blue-600"/>
         </form>
