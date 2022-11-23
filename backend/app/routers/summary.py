0072fb108ddd608e8bbe4392319d231178aad526
james5418 <james900425@gmail.com>
2022-11-24 00:10:02 +0800

    [feat] add get_finish_summary_id with http basic auth for admin page

--- backend/app/routers/summary.py
+++ backend/app/routers/summary.py
@@ -1,11 +1,14 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+import secrets
+import os
 
 from firestore_db import db
 from models import Summary, Questionnaire
 
 summary_router = APIRouter()
 
-@summary_router.get("/undone/{userId}")
+@summary_router.get("/undone/{userId}", response_model=list[str])
 async def get_undone_summary_id(userId: str) -> list[str]:
     docs = db.collection(u'summary').where(u'userId', u'==', userId).where(u'summary', u'==', u'').stream()
     ids = [doc.to_dict()["summaryId"] for doc in docs]
@@ -26,6 +29,32 @@ async def get_summary(userId: str, summaryId: str) -> Summary:
     doc["notification"] = notis
 
     return doc
+
+
+security = HTTPBasic()
+
+@summary_router.get("/finish", response_model=list[str])
+async def get_finish_summary_id(credentials: HTTPBasicCredentials = Depends(security)) -> list[str]:
+    current_password_bytes = credentials.password.encode("utf8")
+    correct_password_bytes = os.environ.get("admin_password").encode("utf8")
+    is_correct_password = secrets.compare_digest(current_password_bytes, correct_password_bytes)
+
+    if not is_correct_password:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    
+    docs = db.collection(u'summary').where(u'userId', u'==', credentials.username).stream()
+    
+    ids = []
+    for doc in docs:
+        doc = doc.to_dict()
+        if doc["summary"] != "":
+            ids.append(doc["summaryId"])
+
+    return ids
 
 
 @summary_router.post("/{userId}/{summaryId}")
