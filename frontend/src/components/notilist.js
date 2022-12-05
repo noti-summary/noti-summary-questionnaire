c04d961b71b1305216aad3b9612c0d3038dc9655
james5418 <james900425@gmail.com>
2022-12-05 16:18:22 +0800

    [feat] add virtualized list on notilist

--- frontend/src/components/notilist.js
+++ frontend/src/components/notilist.js
@@ -1,15 +1,16 @@
 import { useState } from 'react';
-import NotiCard from './notiCard';
-import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
 import Checkbox from '@mui/material/Checkbox';
-
+import Box from '@mui/material/Box';
+import { FixedSizeList } from 'react-window';
+import NotiCard from './notiCard';
 
 export default function NotiList(props) {
-    const [checked, setChecked] = useState([props.notis[0]]);
+
+  const Rows = () => {
+    const [checked, setChecked] = useState([]);
   
     const handleToggle = (value) => () => {
       const currentIndex = checked.indexOf(value);
@@ -21,35 +22,44 @@ export default function NotiList(props) {
         newChecked.splice(currentIndex, 1);
       }
       
-      console.log(newChecked)
       setChecked(newChecked);
     };
 
-  
-    return (
-      <List>
-        {props.notis.map((value) => {
-          const labelId = `checkbox-list-label-${value}`;
-  
-          return (
-            <ListItem key={value}>
-              <ListItemButton role={undefined} onClick={handleToggle(value)} dense>
-                <ListItemIcon>
-                  <Checkbox
-                    edge="start"
-                    checked={checked.indexOf(value) !== -1}
-                    tabIndex={-1}
-                    disableRipple
-                    inputProps={{ 'aria-labelledby': labelId }}
-                  />
-                </ListItemIcon>
-                <NotiCard {...value} key={value.notificationId}/>
-              </ListItemButton>
-            </ListItem>
-          );
-        })}
-      </List>
-    );
+    const renderItems = props.notis.map((value) => 
+      <ListItem key={value}>
+        <ListItemButton role={undefined} onClick={handleToggle(value)} dense>
+          <ListItemIcon>
+            <Checkbox
+              edge="start"
+              checked={checked.indexOf(value) !== -1}
+              tabIndex={-1}
+              disableRipple
+              inputProps={{ 'aria-labelledby': `checkbox-list-label-${value}` }}
+            />
+          </ListItemIcon>
+          <NotiCard {...value} key={value.notificationId}/>
+        </ListItemButton>
+      </ListItem>
+    )
+
+    return renderItems;
   }
+
+  return (
+    <Box
+      sx={{ width: '100%', height: 400, maxWidth: 360, bgcolor: 'background.paper' }}
+    >
+      <FixedSizeList
+        height={400}
+        width={360}
+        itemSize={40}
+        itemCount={1}
+        overscanCount={3}
+      >
+        {Rows}
+      </FixedSizeList>
+    </Box>
+  );
+}
 
   
