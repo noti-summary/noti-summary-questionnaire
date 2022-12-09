0453266645636cf5d80caf9b09ec05e1892ea74e
james5418 <james900425@gmail.com>
2022-12-10 00:53:13 +0800

    [feat] todo page redirection if not login

--- frontend/src/pages/todo/index.js
+++ frontend/src/pages/todo/index.js
@@ -1,4 +1,7 @@
 import { useContext } from 'react';
+import { useRouter } from "next/router";
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { AuthContext } from '../../components/context/authContext';
 import styles from '../../styles/Home.module.css';
 import SummaryList from '../../components/summaryList';
@@ -7,12 +10,31 @@ import SummaryList from '../../components/summaryList';
 export default function ShowSummaryList() {
 
     const context = useContext(AuthContext);
+    const router = useRouter();
+
+    if(!context.isUserLoggedIn()){
+        toast.warn('您尚未登入', {
+            toastId: "not_login",
+            position: "top-center",
+            autoClose: 800,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: false,
+            draggable: false,
+            progress: undefined,
+            theme: "dark",
+            onClose: () => {
+                setTimeout(() => {                
+                    router.push('/login');
+                }, 2000);
+            }
+        });
+    }
     
     return(
         <div className={styles.container}>
-            {context.isUserLoggedIn()
-              ? <SummaryList userId={context.user}/>
-              : <h2>使用本系統前，請先登入</h2>
+            {context.isUserLoggedIn() &&
+              <SummaryList userId={context.user}/>
             }
         </div>
     );
